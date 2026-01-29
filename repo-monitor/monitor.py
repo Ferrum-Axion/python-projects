e5b392a5aecdadc3e5eaee2bbf5a9fa364a94c50
Yaron Segev <yar.segev@gmail.com>
2026-01-29 20:34:47 +0200

    Finished all

--- repo-monitor/monitor.py
+++ repo-monitor/monitor.py
@@ -3,25 +3,38 @@ import sys
 import os
 import yaml
 
-#REPO = "python-projects"
-#OWNER = "Ferrum-Axion"
 
 def configfiledata():
     with open("./config.yaml") as file:
         return yaml.safe_load(file)
 
 
+def checksubmitted(expected_branches, expected_students):
+    not_submitted = []
+    for s in expected_students:
+        if s.lower() not in expected_branches:
+            not_submitted.append(s.lower())
+    return not_submitted    
+
 
 def get_branches():
     config = configfiledata()
+    taskid = config["task_id"]
+
     try:
         response = requests.get(f"https://api.github.com/repos/{config["owner"]}/{config["repo"]}/branches")
         response.raise_for_status()
         data = response.json()
-        if response.status_code != 200:
-            raise
-        branches_name = list(map(lambda b : b["name"], data))
-        print(branches_name)
+        branches_exists = list(map(lambda b : b["name"].lower(), data))
+        branches_expected = list(map(lambda s : f"{taskid}-{s}", config["students"]))
+        missing_subbmissions = checksubmitted(branches_exists, branches_expected)
+        if len(missing_subbmissions) == 0:
+            print("All student submitted the projects")
+        else:
+            print(f"These students did not submitted: {str(missing_subbmissions)}")
+  #      if response.status_code != 200:
+  #          raise
+
     except requests.exceptions.HTTPError as errh:
         print("error is: ",errh)
 
